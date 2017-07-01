efe7013f5f9a12c106d070f45b7a741f94888044
César González Fernández <cesar.gon.fer@gmail.com>
2017-07-01 18:42:32 +0200

    Responsive resize included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isPowerOn : Boolean = false;
+  scale: number = 1;
+
+  constructor() {
+    window.onresize = () => {
+      this.calculateScale();
+    };
+  }
+
+  ngOnInit() {
+    this.calculateScale();
+  }
+
+  calculateScale() {
+    let hScale = window.innerHeight / 920;
+    let wScale = window.innerWidth / 1000;
+
+    this.scale = Math.min(hScale, wScale);
+    console.log("Scale ", this.scale);
+  }
 
   toggle(): Boolean {
     this.isPowerOn = ! this.isPowerOn;
